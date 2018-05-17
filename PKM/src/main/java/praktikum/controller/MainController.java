fe48a2a730b112ca7a2520e1ee236900577054f1
Primoz <primoz98@gmail.com>
2018-05-17 13:09:40 +0200

    MySQL dodan

--- PKM/src/main/java/praktikum/controller/MainController.java
+++ PKM/src/main/java/praktikum/controller/MainController.java
@@ -1,15 +1,20 @@
 package praktikum.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import praktikum.Customer;
+import praktikum.CustomerRepository;
+
 
 @Controller
 public class MainController {
+    @Autowired
+    CustomerRepository repository;
 
-    // inject via application.properties
+
     @Value("${welcome.message}")
     private String message = "Jebela cesta";
 
@@ -24,4 +29,39 @@ public class MainController {
         return "/test";
     }
 
+    @RequestMapping("/save")
+    public String process(){
+        repository.save(new Customer("Jack", "Smith"));
+        repository.save(new Customer("Adam", "Johnson"));
+        repository.save(new Customer("Kim", "Smith"));
+        repository.save(new Customer("David", "Williams"));
+        repository.save(new Customer("Peter", "Davis"));
+        return "Done";
+    }
+
+
+    @RequestMapping("/findall")
+    @ResponseBody
+    public String findAll(){
+        String result = "<html>";
+
+        for(Customer cust : repository.findAll()){
+            result += "<div>" + cust.toString() + "</div>";
+        }
+
+        return result + "</html>";
+    }
+
+
+    @RequestMapping("/findbylastname")
+    @ResponseBody
+    public String fetchDataByLastName(@RequestParam("lastname") String lastName){
+        String result = "<html>";
+
+        for(Customer cust: repository.findByLastName(lastName)){
+            result += "<div>" + cust.toString() + "</div>";
+        }
+
+        return result + "</html>";
+    }
 }
