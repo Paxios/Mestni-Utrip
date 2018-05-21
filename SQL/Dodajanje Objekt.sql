0d9e662dafb0f9e535fe1f2d6d9b5bb2a34b8563
Evixiss <primoz98@gmail.com>
2018-05-21 22:39:39 +0200

    Objekti v Mariboru

---
+++ SQL/Dodajanje Objekt.sql
@@ -0,0 +1,134 @@
+insert into objekt (naziv) 
+values('La Cantina');
+
+insert into objekt (naziv) 
+values('Mc Pekarna');
+
+insert into objekt (naziv) 
+values('Klub mariborskih študentov');
+
+insert into objekt (naziv) 
+values('Štuk');
+
+insert into objekt (naziv) 
+values('Takos lent');
+
+insert into objekt (naziv) 
+values('Bar Trust');
+
+insert into objekt (naziv) 
+values('Fuego - Latino Club');
+
+insert into objekt (naziv) 
+values('Niagara lounge bar');
+
+insert into objekt (naziv) 
+values('Bar Kaos');
+
+insert into objekt (naziv) 
+values('Ribičija');
+
+insert into objekt (naziv) 
+values('Papagayo lounge bar');
+
+insert into objekt (naziv) 
+values('Gostilna Maribor');
+
+insert into objekt (naziv) 
+values('Restavracija Mango');
+
+insert into objekt (naziv) 
+values('Villa Rustica');
+
+insert into objekt (naziv) 
+values('Restavracija Ancora');
+
+insert into objekt (naziv) 
+values('Restavracija Azija');
+
+insert into objekt (naziv) 
+values('La Pizzeria');
+
+insert into objekt (naziv) 
+values('Gostilna Sarajevo');
+
+insert into objekt (naziv) 
+values('Kavarna Q');
+
+insert into objekt (naziv) 
+values('Mc Donalds Europark');
+
+insert into objekt (naziv) 
+values('Pustolovski park Betnava');
+
+insert into objekt (naziv) 
+values('Trije ribniki');
+
+insert into objekt (naziv) 
+values('Mestni Park');
+
+insert into objekt (naziv) 
+values('City Park');
+
+insert into objekt (naziv) 
+values('Magdalenski Park');
+
+insert into objekt (naziv) 
+values('Trg Borisa Kidriča');
+
+insert into objekt (naziv) 
+values('Mariborski grad');
+
+insert into objekt (naziv) 
+values('Glavni trg');
+
+insert into objekt (naziv) 
+values('Kužno znamenje');
+
+insert into objekt (naziv) 
+values('Vodni stolp');
+
+insert into objekt (naziv) 
+values('Sodni stolp');
+
+insert into objekt (naziv) 
+values('Bazilika Matere Usmiljena');
+
+insert into objekt (naziv) 
+values('Slovensko narodno gledališče');
+
+insert into objekt (naziv) 
+values('Slomškov trg');
+
+insert into objekt (naziv) 
+values('Dvorec Betnava');
+
+insert into objekt (naziv) 
+values('Stara Trta');
+
+insert into objekt (naziv) 
+values('Akvarij - terarij');
+
+insert into objekt (naziv) 
+values('Muzej narodne osvoboditve');
+
+insert into objekt (naziv) 
+values('Casino Joker');
+
+insert into objekt (naziv) 
+values('Piramid');
+
+insert into objekt (naziv) 
+values('Vinagova klet');
+
+insert into objekt (naziv) 
+values('Frančiškanska cerkev');
+
+insert into objekt (naziv) 
+values('Umetnostna galerija');
+
+insert into objekt (naziv) 
+values('Lent');
+
+insert into objekt (naziv) 
+values('Pokrajinski muzej');
