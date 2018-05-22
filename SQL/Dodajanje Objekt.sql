ab8e2500b9a3237bdd3c26928e363e3463e7fd21
VnukNejc <33753135+VnukNejc@users.noreply.github.com>
2018-05-22 14:56:37 +0200

    kraj,naslov,objekt
    kraj,naslov,objekt

--- SQL/Dodajanje Objekt.sql
+++ SQL/Dodajanje Objekt.sql
@@ -1,152 +1,154 @@
-insert into objekt (naziv) 
-values('La Cantina');
+insert into objekt (naziv,fk_id_naslov) 
+values('La Cantina',1);
 
-insert into objekt (naziv) 
-values('Mc Pekarna');
+insert into objekt (naziv,fk_id_naslov) 
+values('Mc Pekarna',2);
 
-insert into objekt (naziv) 
-values('Klub mariborskih študentov');
+insert into objekt (naziv,fk_id_naslov)  
+values('Klub mariborskih študentov',3);
 
-insert into objekt (naziv) 
-values('Štuk');
+insert into objekt (naziv,fk_id_naslov)  
+values('Štuk', 4);
 
-insert into objekt (naziv) 
-values('Takos lent');
+insert into objekt (naziv,fk_id_naslov) 
+values('Takos lent', 5);
 
-insert into objekt (naziv) 
-values('Bar Trust');
+insert into objekt (naziv,fk_id_naslov)  
+values('Bar Trust', 6);
 
-insert into objekt (naziv) 
-values('Fuego - Latino Club');
+insert into objekt (naziv,fk_id_naslov) 
+values('Fuego - Latino Club', 7);
 
-insert into objekt (naziv) 
-values('Niagara lounge bar');
+insert into objekt (naziv,fk_id_naslov) 
+values('Niagara lounge bar',8);
 
-insert into objekt (naziv) 
-values('Bar Kaos');
+insert into objekt (naziv,fk_id_naslov)  
+values('Bar Kaos',9);
 
-insert into objekt (naziv) 
-values('Ribičija');
+insert into objekt (naziv,fk_id_naslov) 
+values('Ribičija', 10);
 
-insert into objekt (naziv) 
-values('Papagayo lounge bar');
+insert into objekt (naziv,fk_id_naslov) 
+values('Papagayo lounge bar', 11);
 
-insert into objekt (naziv) 
-values('Gostilna Maribor');
+insert into objekt (naziv,fk_id_naslov) 
+values('Gostilna Maribor', 12);
 
-insert into objekt (naziv) 
-values('Restavracija Mango');
+insert into objekt (naziv,fk_id_naslov) 
+values('Restavracija Mango',13);
 
-insert into objekt (naziv) 
-values('Villa Rustica');
+insert into objekt (naziv,fk_id_naslov) 
+values('Villa Rustica',14);
 
-insert into objekt (naziv) 
-values('Restavracija Ancora');
+insert into objekt (naziv,fk_id_naslov)  
+values('Restavracija Ancora', 15);
 
-insert into objekt (naziv) 
-values('Restavracija Azija');
+insert into objekt (naziv,fk_id_naslov) 
+values('Restavracija Azija', 16);
 
-insert into objekt (naziv) 
-values('Picerija Rožmarin');
+insert into objekt (naziv,fk_id_naslov) 
+values('Picerija Rožmarin', 17);
 
-insert into objekt (naziv) 
-values('Gostilna Sarajevo');
+insert into objekt (naziv,fk_id_naslov) 
+values('Gostilna Sarajevo', 18);
 
-insert into objekt (naziv) 
-values('Kavarna Q');
+insert into objekt (naziv,fk_id_naslov)  
+values('Kavarna Q', 19);
 
-insert into objekt (naziv) 
-values('Mc Donalds Europark');
+insert into objekt (naziv,fk_id_naslov) 
+values('Mc Donalds Europark', 20);
 
-insert into objekt (naziv) 
-values('Pustolovski park Betnava');
+insert into objekt (naziv,fk_id_naslov)  
+values('Pustolovski park Betnava', 21);
 
-insert into objekt (naziv) 
-values('Trije ribniki');
+insert into objekt (naziv,fk_id_naslov) 
+values('Trije ribniki', 22);
 
-insert into objekt (naziv) 
-values('Mestni Park');
+insert into objekt (naziv,fk_id_naslov)  
+values('Mestni Park', 23);
 
-insert into objekt (naziv) 
-values('City Park');
+insert into objekt (naziv,fk_id_naslov) 
+values('City Park', 24);
 
-insert into objekt (naziv) 
-values('Magdalenski Park');
+insert into objekt (naziv,fk_id_naslov)  
+values('Magdalenski Park', 25);
 
-insert into objekt (naziv) 
-values('Trg Borisa Kidriča');
+insert into objekt (naziv,fk_id_naslov) 
+values('Trg Borisa Kidriča', 26);
 
-insert into objekt (naziv) 
-values('Mariborski grad');
+insert into objekt (naziv,fk_id_naslov)  
+values('Mariborski grad', 27);
 
-insert into objekt (naziv) 
-values('Glavni trg');
+insert into objekt (naziv,fk_id_naslov)  
+values('Glavni trg', 28);
 
-insert into objekt (naziv) 
-values('Kužno znamenje');
+insert into objekt (naziv,fk_id_naslov) 
+values('Kužno znamenje', 29);
 
-insert into objekt (naziv) 
-values('Vodni stolp');
+insert into objekt (naziv,fk_id_naslov) 
+values('Vodni stolp', 30);
 
-insert into objekt (naziv) 
-values('Sodni stolp');
+insert into objekt (naziv,fk_id_naslov)  
+values('Sodni stolp', 31);
 
-insert into objekt (naziv)
-values('Bazilika Matere Usmiljenja');
+insert into objekt (naziv,fk_id_naslov) 
+values('Bazilika Matere Usmiljenja', 32);
 
-insert into objekt (naziv) 
-values('Slovensko narodno gledališče');
+insert into objekt (naziv,fk_id_naslov) 
+values('Slovensko narodno gledališče', 33);
 
-insert into objekt (naziv) 
-values('Slomškov trg');
+insert into objekt (naziv,fk_id_naslov) 
+values('Slomškov trg', 34);
 
-insert into objekt (naziv) 
-values('Dvorec Betnava');
+insert into objekt (naziv,fk_id_naslov) 
+values('Dvorec Betnava', 35);
 
-insert into objekt (naziv) 
-values('Stara Trta');
+insert into objekt (naziv,fk_id_naslov) 
+values('Stara Trta', 36);
 
-insert into objekt (naziv) 
-values('Akvarij - terarij');
+insert into objekt (naziv,fk_id_naslov) 
+values('Akvarij - terarij', 37);
 
-insert into objekt (naziv) 
-values('Muzej narodne osvoboditve');
+insert into objekt (naziv,fk_id_naslov) 
+values('Muzej narodne osvoboditve', 38);
 
-insert into objekt (naziv) 
-values('Casino Joker');
+insert into objekt (naziv,fk_id_naslov) 
+values('Casino Joker', 39);
 
-insert into objekt (naziv) 
-values('Piramida');
+insert into objekt (naziv,fk_id_naslov) 
+values('Piramida', 40);
 
-insert into objekt (naziv) 
-values('Vinagova klet');
+insert into objekt (naziv,fk_id_naslov) 
+values('Vinagova klet', 41);
 
-insert into objekt (naziv) 
-values('Umetnostna galerija');
+insert into objekt (naziv,fk_id_naslov) 
+values('Umetnostna galerija', 42);
 
-insert into objekt (naziv) 
-values('Lent');
+insert into objekt (naziv,fk_id_naslov) 
+values('Lent', 43);
 
-insert into objekt (naziv) 
-values('Pokrajinski muzej');
+insert into objekt (naziv,fk_id_naslov) 
+values('Pokrajinski muzej', 44);
 
-insert into objekt (naziv) 
-values('Kavarna Rokaj');
+insert into objekt (naziv,fk_id_naslov) 
+values('Kavarna Rokaj', 45);
 
-insert into objekt (naziv) 
-values('Stadion Ljudski vrt');
+insert into objekt (naziv,fk_id_naslov) 
+values('Stadion Ljudski vrt', 46);
 
-insert into objekt (naaziv)
-values ('Kopališče Pristan');
+insert into objekt (naziv,fk_id_naslov) 
+values ('Kopališče Pristan', 47);
 
-insert into objekt (naziv) 
-values('Europark');
+insert into objekt (naziv,fk_id_naslov) 
+values('Europark', 48);
 
-insert into objekt (naziv) 
-values('Trg Leona Štuklja');
+insert into objekt (naziv,fk_id_naslov) 
+values('Trg Leona Štuklja', 49);
 
-insert into objekt (naziv) 
-values('Kopališče Fontana');
+insert into objekt (naziv,fk_id_naslov) 
+values('Kopališče Fontana', 50);
 
-insert into objekt (naziv) 
-values('Mariborsko Pohorje');
+insert into objekt (naziv,fk_id_naslov)  
+values('Mariborsko Pohorje', 51);
+
+
